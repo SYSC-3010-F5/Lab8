5565c227c7f376d68b565161c4e488e9167d3ae0
henricheung <henricheung@mc6045-19.labs.sce.carleton.ca>
2017-03-10 09:11:34 -0500

    testJunit

---
+++ lab8/src/test/testStudent.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import main.Student;
+
+public class testStudent {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
