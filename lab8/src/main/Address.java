8d090c60832cca1c72588d61901a211339b14369
Nathaniel Vincent Romeo Charlebois <nathaniel.charlebois@gmail.com>
2017-03-10 10:14:35 -0500

    Added logic to Address

--- lab8/src/main/Address.java
+++ lab8/src/main/Address.java
@@ -1,21 +1,64 @@
 package main;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Address
 {
     private int unit;
     private String street;
     private String city;
     private String postalCode;
-
+    
+    private String postalCodeRegex = "^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$";
+    private Pattern regexPattern = Pattern.compile(postalCodeRegex);
+    
+    
     public Address (String street, String city, String postalCode) throws Exception
-    { };
+    { 
+    	this.street = street;
+    	this.city = city;
+    	if(isValidPostalCode(postalCode)){
+    		this.postalCode = postalCode;
+    	}
+    	else{
+    		throw new Exception();
+    	}
+    	
+    };
     public Address (int unit, String street, String city, String postalCode) throws Exception
-    { };
+    { 
+    	this(street,city,postalCode);
+    	this.unit = unit;
+    	
+    };
 
      public String toString() { return null;}
-     public boolean equals(Object o) { return false; }
+     public boolean equals(Object o) {
+    	 if(this == o){
+    		 return true;
+    	 }
+    	 if(o instanceof Address){
+    		 Address address = (Address)o;
+    		 if(street.equals(address.getStreet()) &&
+    				 city.equals(address.getCity()) &&
+    				 postalCode.equals(address.getPostalCode())){
+    			 return true;
+    		 } 
+    		 else{
+    			 return false;
+    		 }
+    	 }
+    	 else{
+    		 return false;
+    	 }
+    	
+     }
      
-     private boolean isValidPostalCode( String postalCode ) { return false; }
+     private boolean isValidPostalCode( String postalCode ) { 
+    	 Matcher matcher = regexPattern.matcher(postalCode);
+    	 return matcher.matches();
+     }
      
      public String getStreet(){
     	 return street;
@@ -36,7 +79,7 @@ public class Address
      public void setCity(String newCity){
     	 city = newCity;
      }
-      
+     
      public void setPostalCode(String newPostalCode){
     	 postalCode = newPostalCode;
      } 
--- lab8/src/test/addressTest.java
+++ lab8/src/test/addressTest.java
@@ -10,7 +10,7 @@ public class addressTest {
 	@Test
 	public void testGetStreet(){
 		try {
-			Address address = new Address("Bank","Ottawa","K1S2R2");
+			Address address = new Address("Bank","Ottawa","K1S 2R2");
 			assertTrue(address.getStreet().equals("Bank"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -21,7 +21,7 @@ public class addressTest {
 	@Test
 	public void testGetCity(){
 		try {
-			Address address = new Address("Bank","Ottawa","K1S2R2");
+			Address address = new Address("Bank","Ottawa","K1S 2R2");
 			assertTrue(address.getCity().equals("Ottawa"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -32,8 +32,8 @@ public class addressTest {
 	@Test
 	public void testGetPostalCode(){
 		try {
-			Address address = new Address("Bank","Ottawa","K1S2R2");
-			assertTrue(address.getPostalCode().equals("K1S2R2"));
+			Address address = new Address("Bank","Ottawa","K1S 2R2");
+			assertTrue(address.getPostalCode().equals("K1S 2R2"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -43,7 +43,7 @@ public class addressTest {
 	@Test
 	public void testSetStreet(){
 		try {
-			Address address = new Address("Bank","Ottawa","K1S2R2");
+			Address address = new Address("Bank","Ottawa","K1S 2R2");
 			address.setStreet("Elgin");
 			assertTrue(address.getStreet().equals("Elgin")); 
 		} catch (Exception e) {
@@ -55,7 +55,7 @@ public class addressTest {
 	@Test
 	public void testSetCity(){
 		try {
-			Address address = new Address("Bank","Ottawa","K1S2R2");
+			Address address = new Address("Bank","Ottawa","K1S 2R2");
 			address.setCity("Toronto");
 			assertTrue(address.getCity().equals("Toronto")); 
 		} catch (Exception e) {
@@ -67,9 +67,9 @@ public class addressTest {
 	@Test 
 	public void testSetPostalCode(){
 		try {
-			Address address = new Address("Bank","Ottawa","K1S2R2");
-			address.setPostalCode("K1S2R4");
-			assertTrue(address.getPostalCode().equals("K1S2R4")); 
+			Address address = new Address("Bank","Ottawa","K1S 2R2");
+			address.setPostalCode("K1S 2R4");
+			assertTrue(address.getPostalCode().equals("K1S 2R4")); 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -79,10 +79,10 @@ public class addressTest {
 	@Test
 	public void testAddress(){
 		try {
-			Address address = new Address("Bank","Ottawa","K1S2R2");
+			Address address = new Address("Bank","Ottawa","K1S 2R2");
 			assertTrue(address.getStreet().equals("Bank"));
 			assertTrue(address.getCity().equals("Ottawa"));
-			assertTrue(address.getPostalCode().equals("K1S2R2"));
+			assertTrue(address.getPostalCode().equals("K1S 2R2"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
