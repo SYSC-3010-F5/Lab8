692956c0da53b988527b5fbe184592db461ae04d
Nathaniel Vincent Romeo Charlebois <nathaniel.charlebois@gmail.com>
2017-03-10 09:08:45 -0500

    Default files

---
+++ lab8/src/main/Name.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Name
+{
+   private String first, last;
+   public Name(String first, String last) {}
+   public String toString() { return null; }
+   public boolean equals(Object o) { return false; }
+}
