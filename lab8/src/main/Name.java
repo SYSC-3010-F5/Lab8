486a35db9f3b90f76f698de718bddc91c798e60e
sarahgarlough <sarahgarlough@mc6045-18.labs.sce.carleton.ca>
2017-03-10 09:59:10 -0500

    Name Class implementation

--- lab8/src/main/Name.java
+++ lab8/src/main/Name.java
@@ -4,6 +4,10 @@ public class Name
 {
    private String first, last;
    public Name(String first, String last) {}
-   public String toString() { return null; }
-   public boolean equals(Object o) { return false; }
+   public String toString() { 
+	String name=first+" "+last;
+	return name;
+   }
+   public boolean equals(Object o) { 
+	   return toString().equals(o); }
 }
